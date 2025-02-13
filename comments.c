133559c238f10372157f1dfd0b0ab80a0bf3e3e8
Junior <eecfredes@gmail.com>
2025-02-12 21:57:49 -0300

    :rocket:comments

---
+++ comments.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+  // Created a variable
+  int data = 34;
+
+  // This code print the variable data
+  printf("Data = %d\n", data);
+
+  // This return only happen if theres no error on this code
+  return EXIT_SUCCESS;
+}
